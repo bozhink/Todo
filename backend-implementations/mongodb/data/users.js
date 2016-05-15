af5f8a5acdc5235d238f26593cbef534843c0ba7
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-15 19:26:44 +0300

    UsersDAO.addUser is implemented.

--- backend-implementations/mongodb/data/users.js
+++ backend-implementations/mongodb/data/users.js
@@ -1,35 +1,72 @@
+var authKeyGenerator = require('../utils/auth-key-generator')
+
 function UserDAO(database) {
     'use strict';
-    
+
     this.db = database;
     this.usersCollection = this.db.collction('users');
-    
-    this.getUsers = function (page, numberOfItemsPerPage, callback) {
+
+    this.getUsers = function(page, numberOfItemsPerPage, callback) {
         page = parseInt(page);
         if (page < 0) {
             throw new Error('Page number should be positive.');
         }
-        
+
         numberOfItemsPerPage = parseInt(numberOfItemsPerPage);
         if (numberOfItemsPerPage < 1) {
             throw new Error('Number of items per page should be greater than 0.')
         }
-        
+
         this.usersCollection.find()
             .sort({
                 'usernameLower': 1
             })
             .skip(page * numberOfItemsPerPage)
             .limit(numberOfItemsPerPage)
-            .toArray(function (err, users) {
+            .toArray(function(err, users) {
                 if (!!err) {
                     throw err;
                 }
-                
+
                 callback(users);
             });
+    }
+
+    this.addUser = function(user, callback) {
+        user.usernameLower = user.username.toLowerCase();
+        user.authKey = authKeyGenerator.get(user.id);
+
+        this.usersCollection.count({
+            'usernameLower': user.usernameLower
+        }, function(err, count) {
+            if (!!err) {
+                throw err;
+            }
+
+            if (count > 0) {
+                callback(null);
+            } else {
+                this.usersCollection.insert({
+                    '_id': user.id,
+                    'username': user.username,
+                    'passHash': user.passHash,
+                    'usernameLower': user.usernameLower,
+                    'authKey': user.authKey
+                }, {
+                    w: 1
+                }, function(err, doc) {
+                    if (!!err) {
+                        throw err;
+                    }
+
+                    callback({
+                        username: user.username,
+                        authKey: user.authKey
+                    });
+                });
+            }
+        });
     }
 }
 
 module.exports.UserDAO = UserDAO;
-
