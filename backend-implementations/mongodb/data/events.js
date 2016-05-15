004be58102ed89f4c418e3c810fc3cd4aeebca2b
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-16 00:06:47 +0300

    EventDAO: get user by its userName.

--- backend-implementations/mongodb/data/events.js
+++ backend-implementations/mongodb/data/events.js
@@ -3,28 +3,46 @@ const DEFAULT_NUMBER_OF_ITEMS_PER_PAGE = 10;
 function EventDAO(database) {
     'use strict';
 
-    eventsCollection = database.collection('events');
+    var eventsCollection = database.collection('events'),
+        usersCollection = database.collection('users');
 
-    this.getEvents = function(userId, page, numberOfItemsPerPage, callback) {
+    this.getEvents = function(userName, page, numberOfItemsPerPage, callback) {
         page = +(page || 0);
         numberOfItemsPerPage = +(numberOfItemsPerPage || DEFAULT_NUMBER_OF_ITEMS_PER_PAGE);
 
-        eventsCollection.find({
-                'creatorId': userId
-            })
-            .skip(page * numberOfItemsPerPage)
-            .limit(numberOfItemsPerPage)
-            .toArray(function(err, events) {
-                if (err != null || events == null) {
-                    callback(null);
-                } else {
-                    callback(events);
-                }
-            });
+        usersCollection.findOne({
+            'usernameLower': userName.toLowerCase()
+        }, function(err, user) {
+            if (err != null || user == null) {
+                callback(null);
+            } else {
+                eventsCollection.find({
+                        'creatorId': user._id.toString()
+                    })
+                    .skip(page * numberOfItemsPerPage)
+                    .limit(numberOfItemsPerPage)
+                    .toArray(function(err, events) {
+                        if (err != null || events == null) {
+                            callback(null);
+                        } else {
+                            callback(events);
+                        }
+                    });
+            }
+        });
+
     };
-    
-    this.addEvent = function(userId, event, callback) {
-        
+
+    this.addEvent = function(userName, eventUsersNames, event, callback) {
+        usersCollection.findOne({
+            'usernameLower': userName.toLowerCase()
+        }, function(err, user){
+            if (err != null || user == null) {
+                callback(null);
+            } else {
+                // TODO: get event users
+            }
+        })
     }
 }
 
