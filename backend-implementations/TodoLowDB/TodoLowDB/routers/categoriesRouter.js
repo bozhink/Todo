35b23204f3e2d46777ae1b4afa9d1203d229e8c0
bozhink <bozhink@gmail.com>
2016-05-20 14:54:12 +0300

    TodoLowDB: categoriesRouter is re-formatted.

--- backend-implementations/TodoLowDB/TodoLowDB/routers/categoriesRouter.js
+++ backend-implementations/TodoLowDB/TodoLowDB/routers/categoriesRouter.js
@@ -1,46 +1,47 @@
 var express = require('express'),
-  _ = require('lodash');
+    _ = require('lodash');
 
 require('../polyfills/array');
 
-module.exports = function(db) {
-  var router = express.Router();
+module.exports = function (db) {
+    var router = express.Router(),
+        usersCollection = db('users');
 
-  router.get('/', function(req, res) {
-    var user = req.user;
-    if (!user) {
-      res.status(401)
-        .json('Not authorized User');
-      return;
-    }
-
-    var categories = db('users')
-      .map(function(user) {
-        var todoCategories = [];
-        if (user.todos) {
-          todoCategories = user.todos.map(function(todo) {
-            return todo.category;
-          });
+    router.get('/', function (req, res) {
+        var categories, user = req.user;
+        if (!user) {
+            res.status(401)
+                .json('Not authorized User');
+            return;
         }
-        var eventCategories = [];
-        if (user.events) {
-          eventCategories = user.events.map(function(event) {
-            return event.category;
-          });
-        }
-        return todoCategories.concat(eventCategories);
-      });
 
-    categories = _.chain(categories)
-      .flatten(categories, true)
-      .sortBy(function(cat) {
-        return cat.toLowerCase();
-      }).uniq()
-      .value();
+        categories = usersCollection.map(function (user) {
+            var todoCategories = [],
+                eventCategories = [];
 
-    res.json({
-      result: categories
+            if (user.todos) {
+                todoCategories = user.todos.map((todo) => todo.category);
+            }
+
+            if (user.events) {
+                eventCategories = user.events.map((event) => event.category);
+            }
+
+            return todoCategories.concat(eventCategories);
+        });
+
+        categories = _.chain(categories)
+            .flatten(categories, true)
+            .sortBy(function (cat) {
+                return cat.toLowerCase();
+            })
+            .uniq()
+            .value();
+
+        res.json({
+            result: categories
+        });
     });
-  });
-  return router;
+
+    return router;
 };
--- backend-implementations/TodoLowDB/TodoLowDB/utils/authorized-user.js
+++ backend-implementations/TodoLowDB/TodoLowDB/utils/authorized-user.js
@@ -1,11 +1,11 @@
 const AUTH_KEY_HEADER_NAME = 'x-auth-key';
-module.exports = function(app, db) {
-  app.use(function(req, res, next) {
-    var authKey = req.headers[AUTH_KEY_HEADER_NAME];
-    var user = db('users').find({
-      authKey: authKey
+module.exports = function (app, db) {
+    app.use(function (req, res, next) {
+        var authKey = req.headers[AUTH_KEY_HEADER_NAME],
+            user = db('users').find({
+                authKey: authKey
+            });
+        req.user = user || null;
+        next();
     });
-    req.user = user || null;
-    next();
-  });
 };
