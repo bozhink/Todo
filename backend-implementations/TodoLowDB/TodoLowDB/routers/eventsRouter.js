e639b17d737c1d3195d720349072dad6b6fafe85
bozhink <bozhink@gmail.com>
2016-05-20 13:04:17 +0300

    TodoLowDB: eventsRouter is corrected.

--- backend-implementations/TodoLowDB/TodoLowDB/routers/eventsRouter.js
+++ backend-implementations/TodoLowDB/TodoLowDB/routers/eventsRouter.js
@@ -1,83 +1,86 @@
 var express = require('express'),
-    idGenerator = require('../utils/id-generator')();
+    uuid = require('uuid');
 
 require('../polyfills/array');
 
 module.exports = function (db) {
-    var router = express.Router();
+    var router = express.Router(),
+        usersCollection = db('users');
 
-    router.get('/', function (req, res) {
-        var user = req.user;
-        if (!user) {
-            res.status(401)
-                .json('Not authorized User');
-            return;
-        }
+    router
+        .get('/', function (req, res) {
+            var events,
+                user = req.user,
+                now = new Date(),
+                indicesToRemove = [];
 
-        var now = new Date();
-        var indicesToRemove = [];
-        user.events = user.events || [];
-
-        user.events.forEach(function (event, index) {
-            var date = new Date(event.date);
-            if (date - now <= 0) {
-                indicesToRemove.push(index);
-            }
-        });
-
-        indicesToRemove.forEach(function (indexToRemove) {
-            user.events.splice(indexToRemove, 1);
-        });
-        db.save();
-
-        user.events.sort(function (e1, e2) {
-            return new Date(e1.date) - new Date(e2.date);
-        });
-
-        var events = user.events || [];
-        events = events.map(function (dbEvent) {
-            var event = {
-                id: dbEvent.id,
-                title: dbEvent.title,
-                category: dbEvent.category,
-                description: dbEvent.description,
-                date: dbEvent.date,
-                creator: db('users').find({
-                    id: dbEvent.creatorId
-                }).username,
-                users: dbEvent.users.map(function (userId) {
-                    return {
-                        id: userId,
-                        username: db('users').find({
-                            id: userId
-                        })
-                    };
-                })
-            };
-            return event;
-        });
-
-        res.json({
-            result: events
-        });
-    })
-        .post('/', function (req, res) {
-            var user = req.user;
             if (!user) {
                 res.status(401)
                     .json('Not authorized User');
                 return;
             }
 
-            var usersUsernames = req.body.users || [];
+            user.events = user.events || [];
+            user.events.forEach(function (event, index) {
+                var date = new Date(event.date);
+                if (date - now <= 0) {
+                    indicesToRemove.push(index);
+                }
+            });
 
-            var users = usersUsernames.map(function (username) {
-                return db('users').find({
+            indicesToRemove.forEach(function (indexToRemove) {
+                user.events.splice(indexToRemove, 1);
+            });
+
+            user.events.sort(function (e1, e2) {
+                return new Date(e1.date) - new Date(e2.date);
+            });
+
+            events = user.events || [];
+            events = events.map(function (dbEvent) {
+                var event = {
+                    id: dbEvent.id,
+                    title: dbEvent.title,
+                    category: dbEvent.category,
+                    description: dbEvent.description,
+                    date: dbEvent.date,
+                    creator: usersCollection.find({
+                        id: dbEvent.creatorId
+                    }).username,
+                    users: dbEvent.users.map(function (userId) {
+                        return {
+                            id: userId,
+                            username: usersCollection.find({
+                                id: userId
+                            })
+                        };
+                    })
+                };
+
+                return event;
+            });
+
+            res.json({
+                result: events
+            });
+        })
+        .post('/', function (req, res) {
+            var event, usersUsernames, users, user = req.user;
+            if (!user) {
+                res.status(401)
+                    .json('Not authorized User');
+                return;
+            }
+
+            usersUsernames = req.body.users || [];
+            users = usersUsernames.map(function (username) {
+                return usersCollection.find({
                     usernameLower: username.toLowerCase()
                 });
             }).filter(function (user) {
                 return !!user;
             });
+
             if (users.length !== usersUsernames.length) {
                 res.status(400)
                     .json('Invalid users added');
@@ -85,11 +88,10 @@ module.exports = function (db) {
             }
 
             users.push(user);
-
             user.events = user.events || [];
 
-            var event = {
-                id: idGenerator.next(),
+            event = {
+                id: uuid(),
                 title: req.body.title,
                 category: req.body.category || 'uncategorized',
                 description: req.body.description,
@@ -104,14 +106,12 @@ module.exports = function (db) {
                 user.events = user.events || [];
                 user.events.push(event);
             });
-            db.save();
-
-            // user.events.push(event);
 
             res.status(201)
                 .json({
                     result: event
                 });
         });
+
     return router;
 };
