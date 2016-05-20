5bbe22f6c852c37f4868ecde2f6a288a240e51f2
bozhink <bozhink@gmail.com>
2016-05-20 16:00:15 +0300

    TodoLowDB: UserDAO.getUserByAuthKey is plugged to authorized-user,js.

--- backend-implementations/TodoLowDB/TodoLowDB/utils/authorized-user.js
+++ backend-implementations/TodoLowDB/TodoLowDB/utils/authorized-user.js
@@ -1,11 +1,17 @@
 const AUTH_KEY_HEADER_NAME = 'x-auth-key';
+
+var UserDAO = require('../data/users').UserDAO;
+
 module.exports = function (app, db) {
+    'use strict';
+
+    var users = new UserDAO(db);
+
     app.use(function (req, res, next) {
-        var authKey = req.headers[AUTH_KEY_HEADER_NAME],
-            user = db('users').find({
-                authKey: authKey
-            });
-        req.user = user || null;
-        next();
+        var authKey = req.headers[AUTH_KEY_HEADER_NAME];
+        users.getUserByAuthKey(authKey, function (user) {
+            req.user = user || null;
+            next();
+        });
     });
 };
