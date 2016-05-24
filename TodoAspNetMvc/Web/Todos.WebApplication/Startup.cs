d432f74e6eb16cca6ad89722d68dfb97be3a6380
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-24 12:36:22 +0300

    TodospNetMvc: Infrastucture is created.

---
+++ TodoAspNetMvc/Web/Todos.WebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Todos.WebApplication.Startup))]
+namespace Todos.WebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
